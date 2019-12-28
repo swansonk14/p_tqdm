656c7f2e6730ca40047c7a4364d434a45ca03c50
Kyle Swanson <swansonk.14@gmail.com>
2019-12-28 02:02:56 +0000

    Updating version to 1.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md') as f:
 
 setup(
     name='p_tqdm',
-    version='1.2',
+    version='1.3',
     author='Kyle Swanson',
     author_email='swansonk.14@gmail.com',
     description='Parallel processing with progress bars',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/swansonk14/p_tqdm',
-    download_url='https://github.com/swansonk14/p_tqdm/v_1.2.tar.gz',
+    download_url='https://github.com/swansonk14/p_tqdm/v_1.3.tar.gz',
     license='MIT',
     packages=find_packages(),
     install_requires=[
@@ -22,8 +22,11 @@ setup(
     test_suite='nose.collector',
     tests_require=['nose'],
     classifiers=[
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
