6a755c888534cc9dc27dd9880495286a9b3152b0
fcoclavero <francisco.clavero@thenotcompany.com>
2021-06-14 11:33:23 -0400

    Use total kwarg as tqdm length if provided

--- p_tqdm/p_tqdm.py
+++ p_tqdm/p_tqdm.py
@@ -38,8 +38,9 @@ def _parallel(ordered: bool, function: Callable, *iterables: Iterable, **kwargs:
     elif type(num_cpus) == float:
         num_cpus = int(round(num_cpus * cpu_count()))
 
-    # Determine length of tqdm (equal to length of shortest iterable)
-    length = min(len(iterable) for iterable in iterables if isinstance(iterable, Sized))
+    # Determine length of tqdm (equal to length of shortest iterable or total kwarg)
+    total = kwargs.pop('total', None)
+    length = total or min(len(iterable) for iterable in iterables if isinstance(iterable, Sized))
 
     # Create parallel generator
     map_type = 'imap' if ordered else 'uimap'
