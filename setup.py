c1a153b2bbf1513b1e042af22933bd4fe41335f7
Kyle Swanson <swansonk.14@gmail.com>
2024-08-04 14:36:09 +0100

    Fixing version numbers in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 # Load version number
-__version__ = None
+__version__: str | None = None
 
 src_dir = Path(__file__).parent.absolute()
 version_file = src_dir / 'p_tqdm' / '_version.py'
@@ -16,14 +16,14 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='p_tqdm',
-    version='1.4.0',
+    version=__version__,
     author='Kyle Swanson',
     author_email='swansonk.14@gmail.com',
     description='Parallel processing with progress bars',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/swansonk14/p_tqdm',
-    download_url='https://github.com/swansonk14/p_tqdm/v_1.3.3.tar.gz',
+    download_url=f'https://github.com/swansonk14/p_tqdm/v_{__version__}.tar.gz',
     license='MIT',
     packages=find_packages(),
     install_requires=[
@@ -34,10 +34,11 @@ setup(
     tests_require=['pytest'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
